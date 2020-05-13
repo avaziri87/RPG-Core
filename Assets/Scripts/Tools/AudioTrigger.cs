aedec9918f183b9b968a373731b8b96e03834457
avaziri87 <alejandrovaziri@gmail.com>
2020-05-12 22:20:08 -0400

    - sound strack added -scene polished -build version 1 (porfolio piece 1)

---
+++ Assets/Scripts/Tools/AudioTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    [SerializeField] AudioClip soundToPlay = null;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<SoundMixer>().FadeOut(soundToPlay);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<SoundMixer>().FadeIn();
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/SoundMixer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMixer : MonoBehaviour
+{
+    [SerializeField] AudioSource original;
+    [SerializeField] AudioSource newToPlay;
+    [SerializeField] float fadeTime = 1f;
+
+    public void FadeOut(AudioClip toPlay)
+    {
+        StartCoroutine(FadeOutOriginal(toPlay));
+    }
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInOriginal());
+    }
+    public IEnumerator FadeOutOriginal(AudioClip toPlay)
+    {
+        newToPlay.clip = toPlay;
+        newToPlay.volume = 0;
+        newToPlay.Play();
+        while(!Mathf.Approximately(original.volume, 0))
+        {
+            original.volume = Mathf.MoveTowards(original.volume, 0, Time.deltaTime / fadeTime);
+            newToPlay.volume = Mathf.MoveTowards(newToPlay.volume, 1, Time.deltaTime / fadeTime);
+        }
+        original.Stop();
+        yield return null;
+    }
+    public IEnumerator FadeInOriginal()
+    {
+        original.volume = 0;
+        original.Play();
+        while (!Mathf.Approximately(original.volume, 1))
+        {
+            original.volume = Mathf.MoveTowards(original.volume, 1, Time.deltaTime / fadeTime);
+            newToPlay.volume = Mathf.MoveTowards(newToPlay.volume, 0, Time.deltaTime / fadeTime);
+        }
+        newToPlay.Stop();
+        yield return null;
+    }
+}
